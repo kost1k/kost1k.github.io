8cc27adf37250c929d2fd57a442f8ce936beb2db
kost1k <kostik048@gmail.com>
2020-03-05 12:12:37 +0300

    Add files via upload

--- 8/js/script.js
+++ 8/js/script.js
@@ -14,16 +14,39 @@ $(function () {
         $playBtn.show();
     });
 
-    var faq = '.faq',
-        faqClose = '.faq__close';
+    var $faq = $('.faq'),
+        faqShow = '.faq-show',
+        faqClose = '.faq__close',
+        $faqTitle = $('.faq__title'),
+        $faqList = $('.faq__list'),
+        $faqItemNum = $('.faq__item-num');
 
-    $(document).on('click', faq, function () {
+    $(document).on('click', faqShow, function () {
+        var screenWidth = $(window).width(),
+            faqItemNumWidth = $faqItemNum.width(),
+            faqListOffset = $faqList[0].getBoundingClientRect(),
+            faqListOffsetTop = faqListOffset.top,
+            faqListOffsetLeft = faqListOffset.left,
+            faqTitleHeight = $faqTitle.height(),
+            faqTitleTop = faqListOffsetTop - faqTitleHeight - 65,
+            faqTitleRight = screenWidth - (faqListOffsetLeft - screenWidth) - (faqItemNumWidth + 20) - 100;
+
+        console.log(faqListOffsetTop)
+
         $(this).addClass('opened');
+        $faq.addClass('opened');
+        $faqTitle.css({
+            top: faqTitleTop,
+            right: faqTitleRight
+        });
     });
 
-    $(document).on('click', faqClose, function (e) {
-        e.stopPropagation();
-
-        $(faq).removeClass('opened');
+    $(document).on('click', faqClose, function () {
+        $faq.removeClass('opened');
+        $(faqShow).removeClass('opened');
+        $faqTitle.css({
+            top: '',
+            right: ''
+        });
     });
 });
