82bcda5a7c2e4a4d67c112c4edc51dda28088ca4
Болдырев Константин <boldyrev@intaro.email>
2020-07-14 14:02:07 +0300

    customize polo test

---
+++ CustomPolo/script.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        colors: [
+            '#C25840',
+            '#F5D3CB',
+            '#8F3D5B',
+            '#4E9E33',
+            '#B7BFAB'
+        ],
+        bodyColor: '#ffffff',
+        neckColor: '#ffffff',
+        cuffColor: '#ffffff',
+
+    }
+});
